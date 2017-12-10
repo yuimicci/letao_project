4ab0efe642c1775398b7bb0809136875eb59c8b1
yuimi <1430936315@qq.com>
2017-12-10 20:23:14 +0800

    完成添加分类，，文件上传预览，基本的pc端后台完成

--- public/manage_my/js/products.js
+++ public/manage_my/js/products.js
@@ -1,30 +1,147 @@
-$(function(){
+$(function () {
     var myPageNum = 1;
     var myPageSize = 5;
-    function getData(){
+
+    function getData() {
         $.ajax({
-            url:"/product/queryProductDetailList",
-            data:{
-                pageSize:myPageSize,
-                page:myPageNum
+            url: "/product/queryProductDetailList",
+            data: {
+                pageSize: myPageSize,
+                page: myPageNum
             },
-            success:function(backData){
-                console.log(backData);
-                $("tbody").html(template("products-tem",backData));
+            success: function (backData) {
+                // console.log(backData);
+                $("tbody").html(template("products-tem", backData));
                 $("#pagintor").bootstrapPaginator({
-                    bootstrapMajorVersion:3,//默认是2，如果是bootstrap3版本，这个参数必填
-                    currentPage:myPageNum,//当前页
-                    totalPages:Math.ceil(backData.total/backData.size),//总页数
-                    size:"small",//设置控件的大小，mini, small, normal,large
-                    onPageClicked:function(event, originalEvent, type,page){
-                      //为按钮绑定点击事件 page:当前点击的按钮值
-                      myPageNum=page;
-                      getData()
+                    bootstrapMajorVersion: 3, //默认是2，如果是bootstrap3版本，这个参数必填
+                    currentPage: myPageNum, //当前页
+                    totalPages: Math.ceil(backData.total / backData.size), //总页数
+                    size: "small", //设置控件的大小，mini, small, normal,large
+                    onPageClicked: function (event, originalEvent, type, page) {
+                        //为按钮绑定点击事件 page:当前点击的按钮值
+                        myPageNum = page;
+                        getData()
                     }
-                  });
-                  
+                });
+
             }
         })
     }
     getData()
+
+
+// 文件上传
+$("#fileupload").fileupload({
+    dataType: "json",
+    //e：事件对象
+    //data：图片上传后的对象，通过e.result.picAddr可以获取上传后的图片地址
+    done: function (e, data) {
+        console.log(data);
+        $('<img src="' + data.result.picAddr + '" style="width:100px;height:100px;">').appendTo("form .form-group:last");
+    }
+});
+
+//   超出 禁止选择图片
+$("#fileupload").click(function (event) {
+    if ($("form .form-group:last img").length == 3) {
+        event.preventDefault();
+        $("form").data('bootstrapValidator').updateStatus("pic1", "VALID");
+    }
+})
+
+// 双击图片删除图片
+$("form .form-group:last").on("dblclick", "img", function () {
+    $(this).remove();
+})
+
+// 初始化表单校验插件
+//使用表单校验插件
+$("form").bootstrapValidator({
+    //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
+    // excluded: [':disabled', ':hidden', ':not(:visible)'],
+
+    //2. 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+        valid: 'glyphicon glyphicon-ok',
+        invalid: 'glyphicon glyphicon-remove',
+        validating: 'glyphicon glyphicon-refresh'
+    },
+
+    //3. 指定校验字段
+    fields: {
+        //校验用户名，对应name表单的name属性
+        proName: {
+            validators: {
+                //不能为空
+                notEmpty: {
+                    message: '产品名称不能为空'
+                }
+            }
+        },
+        proDesc: {
+            validators: {
+                //不能为空
+                notEmpty: {
+                    message: '产品描述不能为空'
+                }
+            }
+        },
+        num: {
+            validators: {
+                //不能为空
+                notEmpty: {
+                    message: '用户库存不能为空'
+                }
+            }
+        },
+        price: {
+            validators: {
+                //不能为空
+                notEmpty: {
+                    message: '价格不能为空'
+                }
+            }
+        },
+        oldPrice: {
+            validators: {
+                //不能为空
+                notEmpty: {
+                    message: '原价格不能为空'
+                }
+            }
+        },
+        size: {
+            validators: {
+                //不能为空
+                notEmpty: {
+                    message: '尺寸不能为空'
+                }
+            }
+        },
+        pic1: {
+            validators: {
+                //不能为空
+                notEmpty: {
+                    message: '图片不能为空'
+                }
+            }
+        }
+    }
+
+}).on('success.form.bv', function (e) {
+    e.preventDefault();
+    //使用ajax提交逻辑
+    $.ajax({
+        url:"/product/addProduct",
+        data:$("form").serialize(),
+        type:"post",
+        success:function(backData){
+            console.log(backData);
+            getData();
+            $(".modal-edit").modal("hide");
+            window.location.reload();
+        }
+    })
+});;
+
 })
--- public/manage_my/js/second.js
+++ public/manage_my/js/second.js
@@ -1,30 +1,130 @@
-$(function(){
-    var mypageNum =1;
+$(function () {
+    var mypageNum = 1;
     var mypageSize = 5;
-    function getData(){
+
+    function getData() {
         $.ajax({
-            url:"/category/querySecondCategoryPaging",
-            data:{
-                pageSize:mypageSize,
-                page:mypageNum
+            url: "/category/querySecondCategoryPaging",
+            data: {
+                pageSize: mypageSize,
+                page: mypageNum
             },
-           success:function(backData){
-               console.log(backData);
-               $("tbody").html(template("second-tem",backData));
-               $("#pagintor").bootstrapPaginator({
-                bootstrapMajorVersion:3,//默认是2，如果是bootstrap3版本，这个参数必填
-                currentPage:mypageNum,//当前页
-                totalPages:Math.ceil(backData.total/backData.size),//总页数
-                size:"small",//设置控件的大小，mini, small, normal,large
-                onPageClicked:function(event, originalEvent, type,page){
-                  //为按钮绑定点击事件 page:当前点击的按钮值
-                  mypageNum = page;
-                  getData();
-                }
-              });
-              
-           }
+            success: function (backData) {
+                //    console.log(backData);
+                $("tbody").html(template("second-tem", backData));
+                $("#pagintor").bootstrapPaginator({
+                    bootstrapMajorVersion: 3, //默认是2，如果是bootstrap3版本，这个参数必填
+                    currentPage: mypageNum, //当前页
+                    totalPages: Math.ceil(backData.total / backData.size), //总页数
+                    size: "small", //设置控件的大小，mini, small, normal,large
+                    onPageClicked: function (event, originalEvent, type, page) {
+                        //为按钮绑定点击事件 page:当前点击的按钮值
+                        mypageNum = page;
+                        getData();
+                    }
+                });
+
+            }
         })
     }
     getData();
+
+
+    $("#fileupload").fileupload({
+        dataType: "json",
+        //e：事件对象
+        //data：图片上传后的对象，通过e.result.picAddr可以获取上传后的图片地址
+        done: function (e, data) {
+            //   console.log(data);
+            //   console.log(data.result.picAddr);
+            $(".form-group>img").attr("src", data.result.picAddr);
+            $("input[name=brandLogo]").val(data.result.picAddr);
+            // 人为更新字段
+            $("form").data('bootstrapValidator').updateStatus("brandLogo", "VALID");
+        }
+    });
+
+    // ajax获取数据  渲染页面
+    var myPageSize = 500;
+    var myPageNum = 1;
+    $.ajax({
+        url: "/category/queryTopCategoryPaging",
+        data: {
+            pageSize: myPageSize,
+            page: myPageNum
+        },
+        success: function (backData) {
+            console.log(backData);
+            // 循环遍历数组
+            $(".dropdown-menu").html("");
+            $.each(backData.rows, function (i, e) {
+                console.log(e.categoryName);
+                $(".dropdown-menu").append('<li><a data-id="' + e.id + '" href="#">' + e.categoryName + '</a></li>');
+            })
+        }
+    })
+
+    // 给动态生成的li列表添加点击事件
+    $(".dropdown-menu").on("click", "li a", function () {
+        var categoryName = $(this).html();
+        // console.log(categoryName);
+        $(".select-value").html(categoryName);
+        $('input[name=categoryId]').val($(this).attr('data-id'));
+        // 人为更新字段
+        $("form").data('bootstrapValidator').updateStatus("categoryId", "VALID");
+    })
+
+    $("form").bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        //3. 指定校验字段
+        fields: {
+            categoryId: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "分类不能为空"
+                    }
+
+                }
+            },
+            brandName: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "分类名不能为空"
+                    }
+                }
+            },
+            brandLogo: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "图片不能为空"
+                    }
+                }
+            }
+        }
+    }).on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+        // console.log("成功了");
+        $.ajax({
+            url: "/category/addSecondCategory",
+            type: "post",
+            data: $("form").serialize(),
+            success: function (backData) {
+                console.log(backData);
+                getData();
+                $(".modal-add").modal("hide");
+                window.location.reload();
+            }
+        })
+    });
 })
